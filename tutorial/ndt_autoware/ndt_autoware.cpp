3a11151d608e5c63d92f1d10f5a0a7b7c817993e
sunmiaozju <sun_miao@zju.edu.cn>
2018-12-07 22:37:56 +0800

    xxx

--- tutorial/ndt_autoware/ndt_autoware.cpp
+++ tutorial/ndt_autoware/ndt_autoware.cpp
@@ -1,38 +1,3 @@
-/*
- *  Copyright (c) 2015, Nagoya University
- *  All rights reserved.
- *
- *  Redistribution and use in source and binary forms, with or without
- *  modification, are permitted provided that the following conditions are met:
- *
- *  * Redistributions of source code must retain the above copyright notice, this
- *    list of conditions and the following disclaimer.
- *
- *  * Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- *
- *  * Neither the name of Autoware nor the names of its
- *    contributors may be used to endorse or promote products derived from
- *    this software without specific prior written permission.
- *
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- *  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
- *  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- *  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- *  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
-
-/*
- Localization and mapping program using Normal Distributions Transform
- Yuki KITSUKAWA
- */
-
 #define OUTPUT  // If you want to output "position_log.txt", "#define OUTPUT".
 
 #include <fstream>
@@ -379,6 +344,7 @@ static double calcDiffForRadian(const double lhs_rad, const double rhs_rad)
     diff_rad = diff_rad + 2 * M_PI;
   return diff_rad;
 }
+
 static void odom_callback(const nav_msgs::Odometry::ConstPtr& input)
 {
   // std::cout << __func__ << std::endl;
@@ -899,6 +865,7 @@ int main(int argc, char** argv)
   added_pose.pitch = 0.0;
   added_pose.yaw = 0.0;
 
+  //current_pose - previous_pose
   diff_x = 0.0;
   diff_y = 0.0;
   diff_z = 0.0;
